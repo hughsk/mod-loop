2e650d9e0a9ffda804ad7239ba3c8d9e820e3f44
Hugh Kennedy <hughskennedy@gmail.com>
2013-07-07 14:05:40 +1000

    Cleaner/faster workaround via @shama

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = loop
 
 function loop(value, divisor) {
-  if (value > 0) return value % divisor
-  value += (~~(value / divisor) + 1) * divisor
-  return value % divisor
+  return ((value % divisor) + divisor) % divisor
 }
